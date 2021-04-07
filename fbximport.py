22401230be8e2d1238d6e30caae3a2d509062ff5
Brandon Withington <32149032+BrandonW24@users.noreply.github.com>
2021-04-07 10:38:32 -0700

    There are troubles getting this to work within the Unreal Engine at the moment.

--- fbximport.py
+++ fbximport.py
@@ -1,23 +1,36 @@
 import os.path
 from unreal_engine.classes import PyFbxFactory
 
-# instantiate a new factory
-fbx_factory = PyFbxFactory()
+def fbximport() :
 
-# build the path for the fbx file
-kaiju_assets_dir = os.path.join(os.path.expanduser('~/Desktop'), 'Kaiju_Assets/Slicer')
+    # instantiate a new factory
+    fbx_factory = PyFbxFactory()
 
-slicer_fbx = os.path.join(kaiju_assets_dir, 'slicer.fbx')
+    fbxFileDirec  = sys.argv[1]
+    fbxFile       = sys.argv[2]
+    meshFile      = sys.argv[3]
 
-# configure the factory
-fbx_factory.ImportUI.bCreatePhysicsAsset = False
-fbx_factory.ImportUI.bImportMaterials = False
-fbx_factory.ImportUI.bImportTextures = False
-fbx_factory.ImportUI.bImportAnimations = False
-# scale the mesh (the Kaiju is 30 meters high !)
-fbx_factory.ImportUI.SkeletalMeshImportData.ImportUniformScale = 0.1;
+# Looking to have this be ran by :
+#  fbximport.py [fbx-file directory] [fbx-file] [mesh-file path]
 
-# import the mesh
-slicer_mesh = fbx_factory.factory_import_object(slicer_fbx, '/Game/Kaiju/Slicer')
+    # build the path for the fbx file
+    fbx_asset_directory = os.path.join(os.path.expanduser('~/Desktop'), 'scans')
+    print(fbx_asset_directory)
+    #slicer_fbx = os.path.join(fbx_asset_directory, fbxFile)
 
-# https://github.com/20tab/UnrealEnginePython/blob/master/tutorials/YourFirstAutomatedPipeline.md
+
+    # configure the factory
+    fbx_factory.ImportUI.bCreatePhysicsAsset = False
+    fbx_factory.ImportUI.bImportMaterials = False
+    fbx_factory.ImportUI.bImportTextures = False
+    fbx_factory.ImportUI.bImportAnimations = False
+    # scale the mesh (the Kaiju is 30 meters high !)
+    fbx_factory.ImportUI.SkeletalMeshImportData.ImportUniformScale = 0.1;
+
+    # import the mesh
+    # slicer_mesh = fbx_factory.factory_import_object(slicer_fbx, meshFile)
+
+    # https://github.com/20tab/UnrealEnginePython/blob/master/tutorials/YourFirstAutomatedPipeline.md
+
+if __name__ == "__main__":
+    build_mesh()
